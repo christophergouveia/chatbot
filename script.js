09c6008ed13e9e81e59ea51a0da33f9b679ebcd6
Christopher <34822892+christophergouveia@users.noreply.github.com>
2024-10-07 09:08:44 -0300

    Update system instruction

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const textarea = document.querySelector("textarea");
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction:
+    "Chat, Você é especializado em jogos de computador, consoles, e jogos retrô. Seu papel é fornecer informações detalhadas, curiosidades, dicas, e orientações sobre uma ampla variedade de jogos, desde os mais clássicos até os mais recentes lançamentos. Seu tom é descontraído e entusiasmado, com uma leve dose de nostalgia, especialmente ao tratar de jogos clássicos ou retrô. Ao discutir jogos mais técnicos ou complexos, mantenha uma abordagem clara e acessível, sem perder o charme de um verdadeiro gamer apaixonado.",
+});
 
 const chat = model.startChat();
 
